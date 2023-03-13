17d8e6108255058b40633d8601f5e7e7816d6cd3
vishwadatt <vishwadattmaharaulji@gmail.com>
2023-03-13 22:30:41 +0530

    all commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-var MongoClient = require('mongodb').MongoClient;
+    var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 let { mongoUrl } = process.env;
 // Connection URL
