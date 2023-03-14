da2c5c29b4b6e9098bc7c68a029ba93899ae5c6e
vishvadatt <107188461+vishvadatt@users.noreply.github.com>
2023-03-14 20:15:31 +0530

    Update database.js
    Add URL

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ let { mongoUrl } = process.env;
 var url = mongoUrl;
 exports.connection = () => {
     return new Promise(async (resolve, reject) => {
-        await MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+        await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
             if (err) console.log("error", err);
             console.log("Database connected");
             var db = client.db('adrixus-db');
